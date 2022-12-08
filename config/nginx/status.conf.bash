54d2c79202a3442bb2fcc7244013e7c076573ecf
houmanjudaki <83509034+houmanjudaki@users.noreply.github.com>
2022-12-08 13:31:42 +0330

    Create status.conf.bash

---
+++ config/nginx/status.conf.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SiteStatus="$1"
+SiteStatusConfig="/etc/nginx/status.conf"
+
+if [ "$SiteStatus" = "maintenance" ]; then
+    echo "root /var/www/maintenance;" > $SiteStatusConfig
+else
+    echo "root /var/www/site;" > $SiteStatusConfig
+fi
+nginx -s reload
